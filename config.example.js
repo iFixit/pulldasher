e28936dd87ad60b3177e89a28763ced894e5e0bf
Daniel Beardsley <daniel@ifixit.com>
2014-03-01 12:59:14 -0800

    Github auth: check that user is a member of org
    Github auth used to just verify if the user has a github account,
    now we verify that they are a member of an organization.

--- config.example.js
+++ config.example.js
@@ -4,9 +4,10 @@ module.exports = {
    github: {
       clientId:     "your github application client id",
       secret:       'your github appliction secret',
-      callbackURL:  'http://localhost:' + port + '/auth/github/callback'
-      token:        'your oauth access token',
-      hook_secret:  'your hook secret'
+      callbackURL:  'http://localhost:' + port + '/auth/github/callback',
+      token:        "oauth api token for server-side api calls",
+      hook_secret:  "some random string to use (?secret=oxwm5gks) to 'secure' github hook handlers"
+      requireOrg:   "Limit access to users belonging to this github organization"
    },
    session: {
       secret: "secret for signing session cookies"
