d55f917615f377bbe4ffbb44f07f4754f468b29a
David Rans <davidr@ifixit.com>
2014-06-10 21:07:33 +0000

    Write signatures to DB

---
+++ models/db_signature.js
@@ -0,0 +1,32 @@
+var _ = require('underscore'),
+    db = require('../lib/db'),
+    Promise = require('promise');
+
+// Builds an object representation of a row in the DB `pulls` table
+// from the data returned by GitHub's API.
+function DBSignature(sigData) {
+   this.data = {
+      number:     sigData.number,
+      user:       sigData.user,
+      // Add a row to DB?
+      // user_id:    sigData.user_id,
+      type:       sigData.type,
+      created_at: sigData.created_at,
+      active:     sigData.active,
+      comment_id: sigData.comment_id
+   };
+}
+
+DBSignature.prototype.save = function() {
+   var sigData = this.data;
+   var q_update = 'INSERT INTO pull_signatures SET ?';
+
+   return new Promise(function(resolve, reject) {
+      db.query(q_update, sigData, function(err, rows) {
+         if (err) { reject(err); }
+         resolve();
+      });
+   });
+};
+
+module.exports = DBSignature;
