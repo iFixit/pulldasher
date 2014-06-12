0c05ab2904c8ed41057d9eb62ab43d24cb5e8f2e
David Rans <davidr@ifixit.com>
2014-06-12 01:05:04 +0000

    Initialize Pull object with Signature objects

--- models/signature.js
+++ models/signature.js
@@ -3,8 +3,7 @@ var config = require('../config');
 /**
  * A block or signoff in a comment.
  */
-function Signature(data)
-{
+function Signature(data) {
    this.data = {
       number:           data.number,
       user:             data.user,
@@ -17,33 +16,29 @@ function Signature(data)
 }
 
 /**
- * Return an array of signatures constructed by parsing github comments for our
- *  custom tags.
+ * Parses a GitHub comment and returns an array of Signature objects.
+ * Returns an empty array if the comment did not contain any of our tags.
  */
-Signature.createFromGithubComments
-   = function createFromGithubComments(comments, pullNumber) {
+Signature.parseComment = function parseComment(comment, pullNumber) {
+   var signatures = [];
+   config.tags.forEach(function(tag) {
+      if (hasTag(comment.body, tag)) {
+         signatures.push(new Signature({
+            number: pullNumber,
+            user: comment.user.login,
+            // avatarUrl = "https://avatars.githubusercontent.com/u/" + user_id
+            user_id: comment.user.id,
+            type: tag,
+            created_at: comment.created_at,
+            // `active` is unknown until all the signatures have been created
+            // and parsed. See Pull.prototype.updateActiveSignatures()
+            active: null,
+            comment_id: comment.id
+         }));
+      }
+   });
 
-   return comments.reduce(function(comments, comment) {
-      config.tags.forEach(function(tag) {
-         if (hasTag(comment.body, tag)) {
-            comments.push(
-             new Signature({
-                number: pullNumber,
-                user: comment.user.login,
-                // avatarUrl = "https://avatars.githubusercontent.com/u/" + user_id
-                user_id: comment.user.id,
-                type: tag,
-                created_at: comment.created_at,
-                // This field is unknown until all the signatures have been created and parsed.
-                // See Pull.prototype.updateActiveSignatures()
-                active: null,
-                comment_id: comment.id
-             }));
-         }
-      });
-
-      return comments;
-   }, []);
+   return signatures;
 }
 
 /**
