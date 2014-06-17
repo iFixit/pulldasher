999aed01222cdde89ebda2eb977fc86769c04c88
Andrew Gilbert <andrewg@ifixit.com>
2014-06-17 21:39:56 +0000

    Write main.js to wire together new features

--- public/main.js
+++ public/main.js
@@ -1,3 +1,23 @@
-define(['pullManager', 'ConnectionManager'],
- function(pullManager, ConnectionManager) {
+define(['jquery', 'underscore', 'pullManager', 'appearanceManager', 'module', 'PullFilter', 'ElementFilter', 'Column', 'ConnectionManager'],
+ function($, _, pullManager, appearanceManager, module, PullFilter, ElementFilter, Column, ConnectionManager) {
+   var spec = View;
+
+
+   var globalPullFilter = new PullFilter(spec);
+   var globalElementFilter = new ElementFilter(spec);
+
+   _.each(spec.columns, function(columnSpec) {
+      var pullFilter = new PullFilter(columnSpec);
+      var elementFilter = new ElementFilter(columnSpec, globalElementFilter);
+      var col = new Column(elementFilter, columnSpec);
+
+      globalPullFilter.onUpdate(pullFilter.update);
+      pullFilter.onUpdate(col.update);
+   });
+
+   pullManager.onUpdate(globalPullFilter.update);
+
+   //$(appearanceManager.addCollapseSwaps);
+   
+   globalPullFilter.update(pullManager.getPulls());
 });
