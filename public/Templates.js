3b6f78cfd02a5c2adc9bee2b9333f69f528d0993
Andrew Gilbert <andrewg@ifixit.com>
2014-06-17 21:42:26 +0000

    Modify view and create template pieces
    Create templates for columns and their restore buttons in Templates.js
    
    Build template HTML and descriptor JSON in index.html for layout.

--- public/Templates.js
+++ public/Templates.js
@@ -3,7 +3,26 @@ define(['underscore'], function(_){
       pull: '\
       <a target="_blank" href="<%- pull.html_url %>" class="list-group-item">\
          <h4 class="list-group-item-heading">#<%- pull.number %> - <%- pull.title %></h4>\
-      </a>'
+      </a>',
+
+      restore: '\
+      <button class="btn btn-primary navbar-btn" style="display: none" type="button">\
+         Restore <em>\
+            <%- this.title %>\
+            <span class="pull-count badge"></span>\
+         </em>\
+      </button>',
+
+      column: '\
+      <div class="panel panel-primary pull-list">\
+            <div class="panel-heading" data-toggle="collapse" data-target="<%- column.id %>">\
+               <h1 class="panel-title">\
+                  <%- column.title %>\
+                  <span class="pull-count badge pull-right"></span>\
+               </h1>\
+            </div>\
+         <div id="<%- column.id %>" class="list-group collapse in"></div>\
+      </div>'
    };
 
    var compiledTemplates = {};
@@ -19,5 +38,5 @@ define(['underscore'], function(_){
          }
          return compiledTemplates[name];
       }
-   }
+   };
 });
