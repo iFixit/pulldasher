646ec84a358755c7c760e0664de4d73876e14371
Andrew Gilbert <andrewg@ifixit.com>
2014-06-17 21:01:40 +0000

    Convert Pull.js to a pure model object
    Most of the meat has moved to Column.js

--- public/Pull.js
+++ public/Pull.js
@@ -1,28 +1,12 @@
-define(['Templates'], function(Templates) {
+define(['underscore'], function(_) {
    var constructor = function(data) {
       _.extend(this, data);
-   }
+   };
 
    _.extend(constructor.prototype, {
-      remove: function() {
-         this.element.remove();
-      },
-   update: function(data) {
-      _.extend(this, data);
-
-      var html = this.render();
-      if (!this.element) {
-         this.element = $(html);
-         // For testing purposes, append to just one column
-         $('#qaPulls').append(this.element);
-      } else {
-         this.element.html(html);
+      update: function(data) {
+         _.extend(this, data);
       }
-   },
-   render: function(templateName, object) {
-      var template = Templates.get(templateName || 'pull');
-      return template(object || this);
-   }
    });
 
    return constructor;
