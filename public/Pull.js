3b83426e42ba048e96f95bc9fb3019e8bade4f4a
Andrew Gilbert <andrewg@ifixit.com>
2014-06-18 18:10:22 +0000

    Add helper functions for checking pull state
    They use the Pull.status object and provide a clearer way to write
    checks of pull states. Also added function to assemble GitHub URL for a
    pull.

--- public/Pull.js
+++ public/Pull.js
@@ -4,8 +4,28 @@ define(['underscore'], function(_) {
    };
 
    _.extend(constructor.prototype, {
+      html_url: function() {
+         return 'https://github.com/' + this.head.repo.owner.login + '/' + this.head.repo.name + '/pull/' + this.number;
+      },
+
       update: function(data) {
          _.extend(this, data);
+      },
+
+      dev_blocked: function() {
+         return this.status.dev_block.length > 0;
+      },
+
+      deploy_blocked: function() {
+         return this.status.deploy_block.length > 0;
+      },
+
+      qa_done: function() {
+         return this.status.QA.length >= this.status.qa_req;
+      },
+
+      cr_done: function() {
+         return this.status.CR.length >= this.status.cr_req;
       }
    });
 
