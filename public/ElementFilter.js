78fd5c73e69efadd6c7cb9a5009bf815faa33231
Andrew Gilbert <andrewg@ifixit.com>
2014-06-25 01:35:21 +0000

    Simplify ElementFilter
    Eliminate unneeded return value check, and add thoughts for later
    extensions.

--- public/ElementFilter.js
+++ public/ElementFilter.js
@@ -5,19 +5,24 @@ define(['underscore'], function(_) {
    /**
     * Constructor
     *
-    * @param theSpec A JavaScript object describing this ElementManager's job
-    * @param theParent An (optional) parent ElementManager which will be used as a source for elements as needed.
+    * @param theSpec A JavaScript object describing this ElementFilter's job
+    * @param theParent An (optional) parent ElementFilter which will be used as a source for elements as needed.
     */
    var constructor = function(spec, prefilter) {
       return {
          filter: function(pull, element) {
+            // Later possibilities: a message object that can carry data from
+            // the prefilter to the current filter, and an array of filters, to
+            // make it cleaner to put multiple things on a pull. The message
+            // object could also be automatically set to tell if there was an
+            // adjust that was run.
+
+            // First, run any "predecessor" filters over it
             if (prefilter) {
-               var updated = prefilter.filter(pull, element);
-               if (updated) {
-                  element = updated;
-               }
+               prefilter.filter(pull, element);
             }
 
+            // Then run the filter (if there is one)
             if (spec.adjust instanceof Function) {
                spec.adjust(pull, element);
             }
