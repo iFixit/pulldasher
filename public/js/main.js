ff59b987f6d09ced9f9dc136c75b76a52c361185
Andrew Gilbert <andrewg@ifixit.com>
2014-07-11 17:43:29 +0000

    Clean up IndicatorFilter to be more general

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ define(['jquery', 'underscore', 'pullManager', 'PullFilter', 'ElementFilter', 'C
  function($, _, pullManager, PullFilter, ElementFilter, Column, spec, IndicatorFilter) {
    var globalPullFilter = new PullFilter(spec);
    var globalElementFilter = new ElementFilter(spec);
-   var globalIndicatorFilter = new IndicatorFilter(spec);
+   var globalIndicatorFilter = new IndicatorFilter(spec.indicators);
 
    _.each(spec.columns, function(columnSpec) {
       _.defaults(columnSpec, {
@@ -15,7 +15,7 @@ define(['jquery', 'underscore', 'pullManager', 'PullFilter', 'ElementFilter', 'C
       var pullFilter = new PullFilter(columnSpec);
 
       var elementFilter = new ElementFilter(columnSpec, globalElementFilter);
-      var indicatorFilter = new IndicatorFilter(columnSpec, globalIndicatorFilter);
+      var indicatorFilter = new IndicatorFilter(columnSpec.indicators, globalIndicatorFilter);
 
       var col = new Column(elementFilter, indicatorFilter, columnSpec);
 
