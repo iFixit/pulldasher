81442a74dfc4e1f21118a9252fc2cd74a0fbf6ab
David Rans <davidr@ifixit.com>
2014-07-15 01:13:36 +0000

    do not open another socket connection

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -1,6 +1,4 @@
-define(['socketjs'], function(io) {
-   var socket = io.connect('/');
-
+define(['socket'], function(socket) {
    $(document).on('click', '.refresh', function() {
       var number = $(this).attr('data-number');
       socket.emit('refresh', number);
