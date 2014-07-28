4870847f7e886d4cf0e77e840ba7587eb1eeae4e
Andrew Gilbert <andrewg@ifixit.com>
2014-07-28 23:55:17 +0000

    Refresh page when unauthenticated
    Refresh the browser when it is unable to authenticate with the server.
    This seems the simplest way to reauthenticate.

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,6 +1,10 @@
 define(['socketjs'], function(io) {
    var socket = io.connect('/');
 
+   socket.on('unauthenticated', function() {
+      window.location.reload();
+   });
+
    socket.on('connect', function() {
       var token = App.socketToken;
       socket.emit('authenticate', token);
