864b931cd987041359c11302836e0f3a27636b7e
Daniel Beardsley <daniel@ifixit.com>
2014-08-13 14:00:31 -0700

    jQuery: add as dependency where it's used
    A few files were using it and jsut expecting it to be there instead
    of listing it as a dependency. This would manifest as a nearly blank
    page (just a header) on some page loads.

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -1,4 +1,4 @@
-define(['socket'], function(socket) {
+define(['jquery', 'socket'], function($, socket) {
    $(document).on('click', '.refresh', function() {
       var number = $(this).attr('data-number');
       socket.emit('refresh', number);
