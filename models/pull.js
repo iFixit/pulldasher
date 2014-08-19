f30aa9876f627b52db942a6b382684a3486fdefb
Josh Terrell <josh@ifixit.com>
2014-08-19 23:49:36 +0000

    Add labels to pulls
    Add new method getPullLabels to git-manager.
    - This method grabs the labels for a pull given a pull number.
    - The data we get from this endpoint isn't complete (yet). Currently this
       endpoint returns an array of labels with the following attributes:
    
              url (to the label defined in the repository), name, color
    
      I'm emailing github support about getting the response containing the user
       who created the label and (possibly) the date the label was added. This
       endpoint is inconsistent with the webhook we get which provides which user
       added/removed the label.
    
    Add an array of labels to the Pull object. This array isn't used at the moment,
     but we're going to want it associated with the pull as a member.

--- models/pull.js
+++ models/pull.js
@@ -4,7 +4,7 @@ var _ = require('underscore');
 var Signature = require('./signature');
 var config = require('../config');
 
-function Pull(data, signatures, comments, commitStatus) {
+function Pull(data, signatures, comments, commitStatus, labels) {
    this.data = {
       number: data.number,
       state: data.state,
@@ -35,6 +35,7 @@ function Pull(data, signatures, comments, commitStatus) {
    this.signatures = signatures || [];
    this.comments = comments || [];
    this.commitStatus = commitStatus;
+   this.labels = labels || [];
 
    // If github pull-data, parse the body for the cr and qa req... else
    // use the values stored in the db.
