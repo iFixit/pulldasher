aae69f8847f07cf952671d31aa2cfd4891e4f266
Andrew Gilbert <andrewg@ifixit.com>
2014-08-28 18:52:42 +0000

    Avoid loosing pulls
    Put pulls which are done with CR and QA but don't have passing tests
    into the new CI column.

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -97,7 +97,7 @@ define(['jquery', 'appearanceUtils'], function($, utils) {
             title: "CI Unsuccessful",
             id: "ciUnsuccessful",
             selector: function(pull) {
-               return pull.build_failed() && !pull.dev_blocked();
+               return !pull.dev_blocked() && (pull.build_failed() || pull.cr_done() && pull.qa_done() && !pull.build_succeeded());
             },
             sort: function(pull) {
                var score = 0;
