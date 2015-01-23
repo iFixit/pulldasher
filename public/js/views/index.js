628709eed4f8e224b23b669cfa13c7052fc7b0c8
Andrew Gilbert <andrewg@ifixit.com>
2015-01-23 19:26:53 +0000

    Move pageIndicators to their own file

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -1,32 +1,8 @@
-define(['jquery', 'appearanceUtils', 'views/columns'], function($, utils, columns) {
+define(['jquery', 'appearanceUtils', 'views/pageIndicators', 'views/columns'], function($, utils, pageIndicators, columns) {
    return {
       navbar: "#restore-buttons",
       page_indicator_box: "#global-indicators",
-      page_indicators: {
-         pull_count: function(pulls, node) {
-            pulls = pulls.filter(function(pull) {
-               return utils.shouldShowPull(pull);
-            });
-            node.text(pulls.length + " open pulls");
-            node.wrapInner('<strong></strong>');
-         },
-         frozen_count: function(pulls, node) {
-            var frozen = pulls.filter(function(pull) {
-               return pull.hasLabel('Cryogenic Storage') && pull.state === 'open';
-            });
-            node.text(frozen.length + " frozen pulls");
-            // If we have frozen pulls, make the count a link.
-            if (frozen.length) {
-               // Pull the repo and org from the first frozen pull.
-               var repo = frozen[0].head.repo.name;
-               var org = frozen[0].head.repo.owner.login;
-               var label = 'Cryogenic Storage';
-               var link = $('<a target="_blank" href="https://www.github.com/' + org + '/' + repo + '/labels/' + label + '"></a>');
-               node.wrapInner(link);
-            }
-            node.wrapInner('<strong></strong>');
-         }
-      },
+      page_indicators: pageIndicators,
       // Global filters
       // where
       selector: function(pull) {
