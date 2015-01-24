9be122e4b44c152dd4d946362e5b63a6075e7ee7
Andrew Gilbert <andrewg@ifixit.com>
2015-01-24 00:27:34 +0000

    Add support for per-template css

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.set('view engine', 'html');
  */
 app.use("/public", express.static(__dirname + '/public'));
 app.use("/spec", express.static(__dirname + '/views/current/spec'));
+app.use("/css", express.static(__dirname + '/views/current/css'));
 app.use("/html", express.static(__dirname + '/views/current/html'));
 app.use(partials());
 app.use(express.bodyParser());
