a0331805c70f0823edf843caf684cb2d0579edfe
Andrew Gilbert <andrewg@ifixit.com>
2015-02-17 21:20:52 +0000

    Basic version of leaderboard
    So that @MutantFreak can still know what's going on without avatars on
    all the pulls.

--- views/standard/spec/pageIndicators.js
+++ views/standard/spec/pageIndicators.js
@@ -1,4 +1,4 @@
-define(['jquery', 'spec/utils', 'appearanceUtils'], function($, utils, aUtils) {
+define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils'], function($, _, utils, aUtils) {
    return {
       pull_count: function(pulls, node) {
          pulls = pulls.filter(utils.shouldShowPull);
@@ -49,7 +49,11 @@ define(['jquery', 'spec/utils', 'appearanceUtils'], function($, utils, aUtils) {
                'realname': instance.realname
             };
          })
-         .each(function(user) {
+         .sortBy(function(item) {
+            return -item.count;
+         })
+         //.first(6)
+         .each(function(user, i) {
             var info = $('<div>');
             info.addClass('leaderboard-posting');
 
@@ -57,15 +61,30 @@ define(['jquery', 'spec/utils', 'appearanceUtils'], function($, utils, aUtils) {
             count.addClass('leaderboard-count');
             count.text(user.count);
 
-            var username = $('<div>');
-            username.addClass('leaderboard-user');
-            username.text(user.realname + ':');
+            //var username = $('<div>');
+            //username.addClass('leaderboard-user');
+            //username.text(user.realname + ':');
+
+
 
             var icon = aUtils.getAvatar(user.id);
+            icon.addClass('leaderboard-avatar');
 
-            info.append(username);
+            //info.append(username);
             info.append(count);
             info.append(icon);
+
+            aUtils.addTooltip(info, user.username);
+
+            if (i < 2) {
+               // First two items
+               info.addClass('leaderboard-leaders');
+            } else if (i < 4) {
+               // Next two items
+               info.addClass('leaderboard-mid');
+            } else {
+               info.addClass('leaderboard-trailing');
+            }
 
             node.append(info);
          });
