7c04bd52b35aa54f20c44a0f278002fd4c40ac7a
Andrew Gilbert <andrewg@ifixit.com>
2015-02-18 19:57:28 +0000

    Improve leaderboard
    More like design, also have two.

--- views/standard/spec/pageIndicators.js
+++ views/standard/spec/pageIndicators.js
@@ -1,4 +1,73 @@
 define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils'], function($, _, utils, aUtils) {
+   var summarize = function(pulls, node, type, extract) {
+      var text = $('<div>');
+      text.addClass('leaderboard-title');
+      text.text(type + ' Leaderboard:');
+
+      node.append(text);
+
+      var date = new Date();
+      var difference = 7 * 24 * 60 * 60 * 1000; // Two weeks in ms
+      var counts = _.chain(pulls)
+      .map(extract)
+      .flatten()
+      // We now have a list of signatures. Now to convert them to usernames
+      .filter(function(signature) {
+        return signature.data.active && (date - (new Date(signature.data.created_at))) < difference;
+      })
+      .map(function(signature) {
+         return signature.data.user;
+      })
+      .groupBy("id")
+      .map(function(group, id) {
+         var instance = group[0];
+         return {
+            'id': id,
+            'count': group.length,
+            'username': instance.login,
+            'realname': instance.realname
+         };
+      })
+      .sortBy(function(item) {
+         return -item.count;
+      })
+      .first(5)
+      .each(function(user, i) {
+         var info = $('<div>');
+         info.addClass('leaderboard-posting');
+
+         var count = $('<div>');
+         count.addClass('leaderboard-count');
+         count.text(user.count);
+
+         //var username = $('<div>');
+         //username.addClass('leaderboard-user');
+         //username.text(user.realname + ':');
+
+
+
+         var icon = aUtils.getAvatar(user.id);
+         icon.addClass('leaderboard-avatar');
+
+         //info.append(username);
+         info.append(count);
+         info.append(icon);
+
+         aUtils.addTooltip(info, user.username);
+
+         if (i < 2) {
+            // First two items
+            info.addClass('leaderboard-leaders');
+         } else if (i < 4) {
+            // Next two items
+            info.addClass('leaderboard-mid');
+         } else {
+            info.addClass('leaderboard-trailing');
+         }
+
+         node.append(info);
+      });
+   };
    return {
       pull_count: function(pulls, node) {
          pulls = pulls.filter(utils.shouldShowPull);
@@ -21,72 +90,14 @@ define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils'], function($, _,
          }
          node.wrapInner('<strong></strong>');
       },
-      leaderboard: function(pulls, node) {
-
-         var text = $('<div>');
-         text.addClass('leaderboard-title');
-         text.text('Leaderboard:');
-
-         node.append(text);
-
-         var counts = _.chain(pulls)
-         .filter(utils.shouldShowPull)
-         .map(function(pull) {
+      cr_leaderboard: function(pulls, node) {
+         summarize(pulls, node, "CR", function(pull) {
             return pull.status.allCR;
-         })
-         .flatten()
-         // We now have a list of signatures. Now to convert them to usernames
-         .map(function(signature) {
-            return signature.data.user;
-         })
-         .groupBy("id")
-         .map(function(group, id) {
-            var instance = group[0];
-            return {
-               'id': id,
-               'count': group.length,
-               'username': instance.login,
-               'realname': instance.realname
-            };
-         })
-         .sortBy(function(item) {
-            return -item.count;
-         })
-         //.first(6)
-         .each(function(user, i) {
-            var info = $('<div>');
-            info.addClass('leaderboard-posting');
-
-            var count = $('<div>');
-            count.addClass('leaderboard-count');
-            count.text(user.count);
-
-            //var username = $('<div>');
-            //username.addClass('leaderboard-user');
-            //username.text(user.realname + ':');
-
-
-
-            var icon = aUtils.getAvatar(user.id);
-            icon.addClass('leaderboard-avatar');
-
-            //info.append(username);
-            info.append(count);
-            info.append(icon);
-
-            aUtils.addTooltip(info, user.username);
-
-            if (i < 2) {
-               // First two items
-               info.addClass('leaderboard-leaders');
-            } else if (i < 4) {
-               // Next two items
-               info.addClass('leaderboard-mid');
-            } else {
-               info.addClass('leaderboard-trailing');
-            }
-
-            node.append(info);
+         });
+      },
+      qa_leaderboard: function(pulls, node) {
+         summarize(pulls, node, "QA", function(pull) {
+            return pull.status.allQA;
          });
       }
    };
