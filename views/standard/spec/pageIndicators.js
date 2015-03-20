e7fb89642eb01cef14268dc3955339b8e3833a7f
Andrew Gilbert <andrewg@ifixit.com>
2015-03-20 19:50:05 +0000

    Move debug indicators to debugIndicators

--- views/standard/spec/pageIndicators.js
+++ views/standard/spec/pageIndicators.js
@@ -1,4 +1,4 @@
-define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils', 'pullManager'], function($, _, utils, aUtils, _manager) {
+define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils'], function($, _, utils, aUtils) {
    var summarize = function(pulls, node, type, extract) {
       // Clean out indicator node. This prevents re-renders from resulting in
       // junk.
@@ -95,17 +95,6 @@ define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils', 'pullManager'],
          summarize(pulls, node, "QA", function(pull) {
             return pull.status.allQA;
          });
-      },
-      rerender: function(pulls, node) {
-         var button = $('<button>Rerender</button>');
-         button.on('click', function() {
-            _manager.trigger();
-         });
-         node.append(button);
-      },
-      rendertime: function(pulls, node) {
-         node.text(new Date());
-         console.log("Last render: " + new Date());
       }
    };
 });
