a6a407e2d9bba7e74dbeeff82356a6018e349cda
Andrew Gilbert <andrewg@ifixit.com>
2015-03-21 00:12:13 +0000

    Improve debug tools
    Per @danielbeardsley's suggestions.

--- views/standard/spec/debugIndicators.js
+++ views/standard/spec/debugIndicators.js
@@ -1,14 +1,20 @@
 define(['jquery', 'underscore', 'spec/utils', 'appearanceUtils', 'pullManager', 'socket'], function($, _, utils, aUtils, _manager, socket) {
    return {
       rerender: function(pulls, node) {
-         var button = $('<button>Rerender</button>');
+         var button = $('<span>');
+         button.addClass('glyphicon glyphicon-blackboard');
          button.on('click', function() {
             _manager.trigger();
          });
+         button.attr('title', 'Rerender page');
+         button.tooltip({'placement': 'auto top'});
          node.append(button);
       },
+
       rendertime: function(pulls, node) {
-         node.text(new Date());
+         node.text((new Date()).toLocaleDateString('en-us', {'month': 'short', 'day': 'numeric', 'hour': 'numeric', 'minute': 'numeric', 'second': 'numeric'}));
+         node.attr('title', 'Date of last rerender');
+         node.tooltip({'placement': 'auto top'});
          console.log("Last render: " + new Date());
       },
 
