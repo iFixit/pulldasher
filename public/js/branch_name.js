ff3f61e12b162561a244408b3739d66c2c663780
Andrew Gilbert <andrewg@ifixit.com>
2015-03-24 17:33:01 +0000

    Move branch_name feature into template
    Keeps parts of it from being in the framework code when they don't need
    to be.

--- public/js/branch_name.js
+++
@@ -1,9 +0,0 @@
-define(['jquery'], function($) {
-   var clipboard = $('<textarea>').attr('id', 'branch_name_clipboard');
-   $('body').append(clipboard);
-
-   $(document).on('mouseenter', '.pull', function() {
-      var branch_name = $(this).attr('data-name');
-      clipboard.val(branch_name).focus().select();
-   });
-});
