3ddd175f607e3c05846531efcdb04bdeb5e44020
Andrew Gilbert <andrewg@ifixit.com>
2015-03-27 16:58:05 +0000

    Make pull signatures get recomputed on update
    When a pull is updated, we need to recompute the list of signatures that
    are current, old, and user. This wasn't happening, due to updates being
    handled through the update method rather than running the constructor
    again. This led to pull signoff marks being inaccurate. Now the
    constructor just calls update, since update does exactly what we wanted
    to do in the constructor anyway.

--- public/js/Pull.js
+++ public/js/Pull.js
@@ -1,41 +1,6 @@
 define(['underscore', 'appearanceUtils'], function(_, utils) {
    var constructor = function(data) {
-      _.extend(this, data);
-
-      var computeSignatures = function(signatures) {
-         var groups = {};
-         var users = {};
-
-         // Contains all signatures that are active
-         groups.current = [];
-         // Contains all signatures that are inactive from users without signatures in current
-         groups.old = [];
-         // Contains the most recent signature from the current user
-         groups.user = null;
-
-         signatures.forEach(function(signature) {
-            if (signature.data.active) {
-               groups.current.push(signature);
-               users[signature.data.user.id] = true;
-
-               if (utils.mySig(signature)) {
-                  groups.user = signature;
-               }
-            } else if (!users[signature.data.user.id]) {
-               groups.old.push(signature);
-               users[signature.data.user.id] = true;
-
-               if (utils.mySig(signature)) {
-                  groups.user = signature;
-               }
-            }
-         });
-
-         return groups;
-      };
-
-      this.cr_signatures = computeSignatures(this.status.allCR);
-      this.qa_signatures = computeSignatures(this.status.allQA);
+      this.update(data);
    };
 
    _.extend(constructor.prototype, {
@@ -44,7 +9,42 @@ define(['underscore', 'appearanceUtils'], function(_, utils) {
       },
 
       update: function(data) {
+         var computeSignatures = function(signatures) {
+            var groups = {};
+            var users = {};
+
+            // Contains all signatures that are active
+            groups.current = [];
+            // Contains all signatures that are inactive from users without signatures in current
+            groups.old = [];
+            // Contains the most recent signature from the current user
+            groups.user = null;
+
+            signatures.forEach(function(signature) {
+               if (signature.data.active) {
+                  groups.current.push(signature);
+                  users[signature.data.user.id] = true;
+
+                  if (utils.mySig(signature)) {
+                     groups.user = signature;
+                  }
+               } else if (!users[signature.data.user.id]) {
+                  groups.old.push(signature);
+                  users[signature.data.user.id] = true;
+
+                  if (utils.mySig(signature)) {
+                     groups.user = signature;
+                  }
+               }
+            });
+
+            return groups;
+         };
+
          _.extend(this, data);
+
+         this.cr_signatures = computeSignatures(this.status.allCR);
+         this.qa_signatures = computeSignatures(this.status.allQA);
       },
 
       dev_blocked: function() {
