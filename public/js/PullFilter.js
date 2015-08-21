d4b1bbe4e3a7165ed11b83d93de58aa51697359d
Andrew Gilbert <andrewg@ifixit.com>
2015-08-20 18:07:12 -0700

    Support multiple filter functions in config
    We can now use an array of functions rather than just one function in the config
    for filtering, sorting, and grouping. The functions will be chained, with the
    first receiving the full list of data, then the second receiving the filtered
    list resulting from the first, then the third receiving the filtered list
    resulting from the second, and so on. The same thing will happen with sorting
    and grouping functions, but the results are probably more or less meaningless:
    unless we have a stable sort, for example (which I don't believe we do),
    multiple sorting won't be guaranteed to mean anything.

--- public/js/PullFilter.js
+++ public/js/PullFilter.js
@@ -20,15 +20,19 @@ define(['underscore'], function(_) {
       // Only run func on obj and maybeFunction if maybeFunction is a function.
       // If maybeFunction is a function, return the result of running func
       // If maybeFunction is not a function, return obj.
-      // Used to (potentially) run Bootstrap functions across an array of
-      // pulls, where the Bootstrap function needs a function as an argument,
+      // Used to (potentially) run Underscore functions across an array of
+      // pulls, where the Underscore function needs a function as an argument,
       // but I can't be sure if there will be any function to give it. Then
-      // what I do is I pass the Bootstrap function as func and my function as
+      // what I do is I pass the Underscore function as func and my function as
       // maybeFunction, and if my function exists, then it will run the
-      // Bootstrap function with it.
+      // Underscore function with it.
       var tryApply = function(obj, func, maybeFunction) {
          if (maybeFunction instanceof Function) {
             return func(obj, maybeFunction);
+         } else if (maybeFunction instanceof Array) {
+            return _.reduce(maybeFunction, function(obj, maybeFunction) {
+               return tryApply(obj, func, maybeFunction);
+            }, obj);
          }
          return obj;
       };
