b4ef3f1a367bf18de160080fd1311570fa57320b
Chris Opperwall <chriso@ifixit.com>
2017-08-17 23:16:40 -0700

    Sleep before attempting to migrate db
    This causes an error when starting docker-compose with an already
    initialized db, since the web ap always tries to start before MySQL is
    ready. By waiting first, the first try should almost always succeed.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 
 # Wait for the DB to start accepting connections.
 for i in $(seq 5); do
+   sleep 10
    echo "Attempt $i"
-   bin/migrate && break || sleep 10
+   bin/migrate && break
 done
 
 echo "Setup done, starting pulldasher..."
