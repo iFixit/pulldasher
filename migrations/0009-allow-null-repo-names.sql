c180f39078c2f7596ed2766abdf64f6a333b5779
Guy Moore <guy.moore.normal@gmail.com>
2017-08-26 19:08:47 +0000

    Remove alters since these columns are going to be dropped

--- migrations/0009-allow-null-repo-names.sql
+++ migrations/0009-allow-null-repo-names.sql
@@ -1,6 +1,2 @@
-ALTER TABLE comments MODIFY COLUMN repo_name varchar(255) NULL;
-ALTER TABLE pull_labels MODIFY COLUMN repo_name varchar(255);
-ALTER TABLE pulls MODIFY COLUMN repo_name varchar(255);
-
 ALTER TABLE pulls DROP KEY pulls_repo;
 ALTER TABLE pulls ADD KEY pulls_repo (repo);
