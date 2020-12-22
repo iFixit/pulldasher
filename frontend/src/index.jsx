aba5e51fa1f21b0e958a213cdf1c65c315203be6
Daniel Beardsley <daniel@ifixit.com>
2020-12-21 23:55:24 -0800

    Reaaaaaaactt! Bare bones empty shell of a react app
    This will be the starting point of a new frontend for pulldasher.
    We'll copy over (and *maybe* reuse) some of the old code as needed.

---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PullDasherUI from './pulldasher.jsx';
+
+const root = document.createElement("div");
+document.body.appendChild(root);
+ReactDOM.render(<PullDasherUI/>, root);
---
+++ frontend/webpack.dev.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const relative = (pathPart) => path.resolve(__dirname, pathPart);
+
+module.exports = {
+   module: {
+      rules: [
+         {
+            // for any file with a suffix of js or jsx
+            test: /\.jsx?$/,
+            // ignore transpiling JavaScript from node_modules as it should be that state
+            exclude: /node_modules/,
+            // use the babel-loader for transpiling JavaScript to a suitable format
+            loader: 'babel-loader',
+            options: {
+               // attach the presets to the loader (most projects use .babelrc file instead)
+               presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+         }
+      ]
+   },
+   resolve: {
+      modules: [relative('../node_modules')]
+   },
+   output: {
+      path: relative("dist"),
+   },
+   entry: {
+      "main": relative("src/index.jsx")
+   },
+   plugins: [
+      new HtmlWebpackPlugin()
+   ],
+   mode: 'development',
+   devtool: 'eval-cheap-module-source-map',
+};
