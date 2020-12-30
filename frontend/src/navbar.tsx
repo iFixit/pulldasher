7f76382e3a0ff4325238d24cdcac23f3d2f1407b
Daniel Beardsley <daniel@ifixit.com>
2020-12-30 02:01:26 -0800

    Navbar: add pull count

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -3,12 +3,22 @@ import { useContext } from 'react';
 import PullsContext from './pulls-context';
 import { Pull } from './types';
 import Navbar from 'react-bootstrap/Navbar';
+import styled from 'styled-components';
+
+const Brand = styled(Navbar.Brand)`
+   width: 200px;
+   position: absolute;
+   margin: auto;
+   left: 0;
+   right: 0;
+`;
 
 export default function() {
    const pulls: Pull[] = useContext(PullsContext).pulls;
    return (
-      <Navbar expand="sm">
-         <Navbar.Brand>Pulldasher</Navbar.Brand>
+      <Navbar bg="dark" expand="sm" variant="dark">
+         <Navbar.Text>{pulls.length} open</Navbar.Text>
+         <Brand>Pulldasher</Brand>
          <Navbar.Toggle/>
       </Navbar>
    );
