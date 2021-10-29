cde6e1499cf4fa5424651776a91db182b1d662bd
Daniel Beardsley <daniel@ifixit.com>
2021-10-28 23:15:58 -0600

    PullCard: add CR / QA count
    Copied over some of this logic from the pull class on the old frontend.

--- frontend/src/pull-card.tsx
+++ frontend/src/pull-card.tsx
@@ -5,6 +5,10 @@ export default function PullCard({pull}: {pull: Pull}) {
    return (
       <ListGroup.Item as='a' href={pull.getUrl()}>
          {pull.title}
+         <div>
+            <span>CRs: {pull.cr_signatures.current.length}</span>&nbsp;
+            <span>QAs: {pull.qa_signatures.current.length}</span>
+         </div>
       </ListGroup.Item>
    );
 }
--- frontend/src/pull.ts
+++ frontend/src/pull.ts
@@ -1,10 +1,25 @@
 import { extend } from "underscore";
-import { PullData } from "./types";
+import { PullData, Signature } from "./types";
+
+interface SignatureGroup {
+   // Contains all signatures that are active
+   current: Signature[],
+   // Contains all signatures that are inactive from users without signatures in current
+   old: Signature[],
+   // Contains the most recent signature from the current user
+   user: Signature | null
+};
 
 export class Pull extends PullData {
+   cr_signatures: SignatureGroup;
+   qa_signatures: SignatureGroup;
+
    constructor(data: PullData) {
       super();
       extend(this, data);
+
+      this.cr_signatures = computeSignatures(data.status.allCR);
+      this.qa_signatures = computeSignatures(data.status.allQA);
    }
 
    getUrl(): string {
@@ -20,3 +35,28 @@ export class Pull extends PullData {
       return this.status.CR.length >= this.status.cr_req;
    }
 }
+
+function computeSignatures(signatures: Signature[]): SignatureGroup {
+   var groups: SignatureGroup = {
+      current: [],
+      old: [],
+      user: null
+   };
+   var users: Record<number, boolean> = {};
+
+   signatures.forEach(function(signature) {
+      if (users[signature.data.user.id]) {
+         return;
+      }
+
+      if (signature.data.active) {
+         groups.current.push(signature);
+      } else {
+         groups.old.push(signature);
+      }
+
+      users[signature.data.user.id] = true;
+   });
+
+   return groups;
+};
