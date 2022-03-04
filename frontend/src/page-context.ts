7eb794731ce3b5204c704d8fb5080d782a61229d
Daniel Beardsley <daniel@ifixit.com>
2022-03-04 00:45:25 -0700

    frontend page-context: add getUser() function
    So components anywhere can use the info about the current user.

--- frontend/src/page-context.ts
+++ frontend/src/page-context.ts
@@ -1,7 +1,16 @@
 interface tokenResponse {
-   socketToken: string
+   socketToken: string,
+   user: string,
 }
+
+let pageContext: tokenResponse;
+
+export function getUser() {
+   return pageContext && pageContext.user;
+}
+
 export default function getPageContext(): Promise<tokenResponse> {
    return fetch("/token")
-      .then((response) => response.json());
+      .then((response) => response.json())
+      .then((response) => pageContext = response);
 }
