88dcef8aa2d1a1ea44098214855c6f1ead2a458c
Daniel Beardsley <daniel@ifixit.com>
2022-03-06 02:14:01 -0700

    ChakraProvider: move it up a level too
    Mostly so the Pulldasher UI doesn't have to think about it.
    
    The Pulldasher component is now just about content, no boilerplate.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,8 @@
 import { render } from 'react-dom';
 import { Pulldasher } from './pulldasher';
 import { PullsProvider } from './pulls-context';
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from './theme';
 import '../../views/standard/less/themes/day_theme.less';
 import '../../views/standard/less/themes/night_theme.less';
 
@@ -10,7 +12,9 @@ document.body.appendChild(root);
 function App() {
    return (
    <PullsProvider>
-      <Pulldasher/>
+      <ChakraProvider theme={theme}>
+         <Pulldasher/>
+      </ChakraProvider>
    </PullsProvider>);
 }
 render(<App/>, root);
--- frontend/src/pulldasher.tsx
+++ frontend/src/pulldasher.tsx
@@ -1,10 +1,8 @@
 import { usePulls } from './pulls-context';
-import { ChakraProvider } from "@chakra-ui/react"
 import { Pull } from './pull';
 import { Navbar } from './navbar';
 import { Column } from './column';
 import { Box, SimpleGrid } from "@chakra-ui/react"
-import { theme } from './theme';
 
 export const Pulldasher: React.FC = function() {
    const pulls: Pull[] = usePulls();
@@ -14,35 +12,35 @@ export const Pulldasher: React.FC = function() {
    const pullsDevBlocked = pulls.filter(pull => pull.isDevBlocked());
    const pullsNeedingCR = pulls.filter(pull => !pull.isCrDone());
    const pullsNeedingQA = pulls.filter(pull => !pull.isQaDone());
-   return (<ChakraProvider theme={theme}>
-         <Navbar mb={4}/>
-         <Box maxW={1024} m="auto" px="var(--body-gutter)">
-         <SimpleGrid columns={3} spacing="var(--body-gutter)">
-            <Box>QA Leaderboard</Box>
-            <Box>CR Leaderboard</Box>
-         </SimpleGrid>
-         <SimpleGrid columns={3} spacing="var(--body-gutter)">
-            <Box>
-               <Column title="CI Blocked" variant="ciBlocked" pulls={pullsCIBlocked}/>
-            </Box>
-            <Box>
-               <Column title="Deploy Blocked" variant="deployBlocked" pulls={pullsDeployBlocked}/>
-            </Box>
-            <Box>
-               <Column title="Ready" variant="ready" pulls={pullsReady}/>
-            </Box>
-         </SimpleGrid>
-         <SimpleGrid columns={3} spacing="var(--body-gutter)">
-            <Box>
-               <Column title="Dev Block" pulls={pullsDevBlocked}/>
-            </Box>
-            <Box>
-               <Column title="CR" pulls={pullsNeedingCR}/>
-            </Box>
-            <Box>
-               <Column title="QA" pulls={pullsNeedingQA}/>
-            </Box>
-         </SimpleGrid>
+   return (<>
+      <Navbar mb={4}/>
+      <Box maxW={1024} m="auto" px="var(--body-gutter)">
+      <SimpleGrid columns={3} spacing="var(--body-gutter)">
+         <Box>QA Leaderboard</Box>
+         <Box>CR Leaderboard</Box>
+      </SimpleGrid>
+      <SimpleGrid columns={3} spacing="var(--body-gutter)">
+         <Box>
+            <Column title="CI Blocked" variant="ciBlocked" pulls={pullsCIBlocked}/>
          </Box>
-   </ChakraProvider>);
+         <Box>
+            <Column title="Deploy Blocked" variant="deployBlocked" pulls={pullsDeployBlocked}/>
+         </Box>
+         <Box>
+            <Column title="Ready" variant="ready" pulls={pullsReady}/>
+         </Box>
+      </SimpleGrid>
+      <SimpleGrid columns={3} spacing="var(--body-gutter)">
+         <Box>
+            <Column title="Dev Block" pulls={pullsDevBlocked}/>
+         </Box>
+         <Box>
+            <Column title="CR" pulls={pullsNeedingCR}/>
+         </Box>
+         <Box>
+            <Column title="QA" pulls={pullsNeedingQA}/>
+         </Box>
+      </SimpleGrid>
+      </Box>
+   </>);
 }
