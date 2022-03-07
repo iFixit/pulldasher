b2ddd2e47fe0a10af29ebabdf4d74e1a5971d1fd
Daniel Beardsley <daniel@ifixit.com>
2022-03-07 14:22:51 -0700

    pulls-context: use filtered-pulls-state
    Now there's a filtering layer that can be modified via the
    context.setFilter() function that will affect all consumers.

--- frontend/src/pulls-context.tsx
+++ frontend/src/pulls-context.tsx
@@ -1,20 +1,33 @@
 import { createContext, useContext } from 'react';
-import { usePullsState } from './pulls-state';
+import { useFilteredPullsState, FilterFunction, FilterFunctionSetter } from './filtered-pulls-state';
 import { Pull } from './pull';
 
 interface PullContextProps {
+   // Array of pulls passing the filter function
    pulls: Pull[];
+   // Changes the filter function
+   setFilter: FilterFunctionSetter;
 }
 
-export const PullsContext = createContext<PullContextProps>({pulls:[]});
+const defaultProps = {
+   pulls: [],
+   // Default implementation is a no-op, just so there's
+   // something there until the provider is used
+   setFilter: (filter: FilterFunction) => filter,
+}
+export const PullsContext = createContext<PullContextProps>(defaultProps);
 
 export function usePulls(): Pull[] {
    return useContext(PullsContext).pulls;
 }
 
-export const PullsProvider = function({children}) {
-   const pulls: Pull[] = usePullsState();
-   return (<PullsContext.Provider value={{pulls:pulls}}>
+export function useSetFilter(): FilterFunctionSetter {
+   return useContext(PullsContext).setFilter;
+}
+
+export const PullsProvider = function({children}: {children: React.ReactNode}) {
+   const [pulls, setFilter] = useFilteredPullsState();
+   return (<PullsContext.Provider value={{pulls, setFilter}}>
       {children}
    </PullsContext.Provider>);
 }
