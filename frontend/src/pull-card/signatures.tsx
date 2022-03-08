d3eee94332132a3540805faa8b4304af66e4a958
Daniel Beardsley <daniel@ifixit.com>
2022-03-07 23:59:24 -0700

    pull-car/sigs: extract some functions

--- frontend/src/pull-card/signatures.tsx
+++ frontend/src/pull-card/signatures.tsx
@@ -29,25 +29,35 @@ export function Signatures({pull, title, signatures, required}: SignaturesProps)
          sx={styles}
       >
       <Box mr={2}>{title}</Box>
-      {allSignatures.map((sig) => {
-         return (<FontAwesomeIcon
-            fontSize="18px"
-            color={colorForSignature(sig)}
-            key={sig.data.comment_id}
-            title={sig.data.user.login}
-            className="build_status"
-            icon={faCheckCircle}/>)
-      })}
-      {[...new Array(unfulfilledCount)].map((_, i) => {
-         return (<FontAwesomeIcon
-            fontSize="18px"
-            color="var(--signature-default)"
-            key={"empty" + i}
-            className="build_status"
-            icon={faCheckCircle}/>)
-      })}
+      {NewAndOldSigs(allSignatures)}
+      {UnfullfilledSigs(unfulfilledCount)}
    </HStack>
    );
+}
+
+function NewAndOldSigs(allSignatures: Signature[]) {
+   return allSignatures.map((sig) => {
+      return (<FontAwesomeIcon
+         fontSize="18px"
+         color={colorForSignature(sig)}
+         key={sig.data.comment_id}
+         title={sig.data.user.login}
+         className="build_status"
+         icon={faCheckCircle}/>)
+   });
+}
+
+function UnfullfilledSigs(count: number) {
+   const sigs = new Array(count);
+   for (let i = 0; i < count; i++) {
+      sigs[i] = (<FontAwesomeIcon
+         fontSize="18px"
+         color="var(--signature-default)"
+         key={"empty" + i}
+         className="build_status"
+         icon={faCheckCircle}/>);
+   }
+   return sigs;
 }
 
 function colorForSignature(sig: Signature): string {
