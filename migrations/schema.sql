1c25a693afe37f71d29c5a73957faf18aeb2cc19
Daniel Beardsley <daniel@ifixit.com>
2022-03-10 14:54:26 -0700

    schema.sql commit diff after utf8mb4 conversion

--- migrations/schema.sql
+++ migrations/schema.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.23-14, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.27-18, for Linux (x86_64)
 --
 -- Host: db.cominor.com    Database: metrics
 -- ------------------------------------------------------
--- Server version	8.0.23-14
+-- Server version	8.0.27-18
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,16 +30,16 @@
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `comments` (
-  `repo` varchar(255) NOT NULL DEFAULT '',
-  `comment_type` enum('issue','review') NOT NULL DEFAULT 'issue',
+CREATE TABLE IF NOT EXISTS `comments` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `comment_type` enum('issue','review') COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'issue',
   `comment_id` int unsigned NOT NULL,
   `number` int unsigned NOT NULL,
-  `user` varchar(255) NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date` int unsigned DEFAULT NULL,
   PRIMARY KEY (`repo`,`comment_type`,`comment_id`),
   KEY `pull` (`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -48,16 +48,16 @@ CREATE TABLE `comments` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `commit_statuses` (
-  `repo` varchar(255) NOT NULL DEFAULT '',
-  `commit` char(40) NOT NULL,
-  `state` enum('pending','success','error','failure') NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `log_url` varchar(255) NOT NULL,
-  `context` varchar(255) NOT NULL DEFAULT 'default',
+CREATE TABLE IF NOT EXISTS `commit_statuses` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
+  `commit` char(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `state` enum('pending','success','error','failure') COLLATE utf8mb4_general_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `log_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `context` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'default',
   PRIMARY KEY (`repo`,`commit`,`context`),
   KEY `commit_statuses_state` (`state`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -66,19 +66,19 @@ CREATE TABLE `commit_statuses` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `issues` (
-  `repo` varchar(255) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS `issues` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `number` int NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `difficulty` int DEFAULT NULL,
-  `milestone_title` varchar(255) DEFAULT NULL,
+  `milestone_title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `milestone_due_on` int DEFAULT NULL,
-  `assignee` varchar(255) DEFAULT NULL,
-  `status` varchar(30) DEFAULT NULL,
+  `assignee` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `status` varchar(30) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `date_closed` int DEFAULT NULL,
   `date_created` int DEFAULT NULL,
   PRIMARY KEY (`repo`,`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,15 +87,15 @@ CREATE TABLE `issues` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pull_labels` (
-  `repo` varchar(255) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS `pull_labels` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `number` int unsigned NOT NULL,
-  `title` varchar(32) NOT NULL,
-  `user` varchar(255) DEFAULT NULL,
+  `title` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `date` int unsigned DEFAULT NULL,
   PRIMARY KEY (`repo`,`number`,`title`),
   KEY `pull_labels_title` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,18 +104,18 @@ CREATE TABLE `pull_labels` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pull_signatures` (
-  `repo` varchar(255) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `pull_signatures` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `number` int unsigned NOT NULL,
-  `user` varchar(255) NOT NULL,
-  `type` varchar(32) NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `type` varchar(32) COLLATE utf8mb4_general_ci NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1',
   `comment_id` int unsigned NOT NULL,
   `userid` int unsigned NOT NULL,
   `date` int unsigned DEFAULT NULL,
   KEY `pull_signatures_number` (`repo`,`number`,`active`),
   KEY `pull_signatures_type` (`repo`,`user`,`type`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -124,23 +124,23 @@ CREATE TABLE `pull_signatures` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `pulls` (
-  `repo` varchar(255) NOT NULL DEFAULT '',
+CREATE TABLE IF NOT EXISTS `pulls` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL DEFAULT '',
   `number` int unsigned NOT NULL,
-  `state` enum('open','closed') NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `body` text NOT NULL,
-  `head_branch` varchar(255) NOT NULL,
-  `head_sha` char(40) NOT NULL,
-  `base_branch` varchar(255) NOT NULL,
-  `owner` varchar(255) NOT NULL,
+  `state` enum('open','closed') COLLATE utf8mb4_general_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `body` text COLLATE utf8mb4_general_ci NOT NULL,
+  `head_branch` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `head_sha` char(40) COLLATE utf8mb4_general_ci NOT NULL,
+  `base_branch` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `owner` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `cr_req` int NOT NULL DEFAULT '2',
   `qa_req` int NOT NULL DEFAULT '1',
   `date` int unsigned DEFAULT NULL,
   `date_updated` int unsigned DEFAULT NULL,
   `date_closed` int unsigned DEFAULT NULL,
   `date_merged` int unsigned DEFAULT NULL,
-  `milestone_title` varchar(255) DEFAULT NULL,
+  `milestone_title` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `milestone_due_on` int unsigned DEFAULT NULL,
   `closes` int unsigned DEFAULT NULL,
   `connects` int unsigned DEFAULT NULL,
@@ -152,7 +152,7 @@ CREATE TABLE `pulls` (
   PRIMARY KEY (`repo`,`number`),
   KEY `pulls_state` (`state`),
   KEY `pulls_repo` (`repo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -161,17 +161,17 @@ CREATE TABLE `pulls` (
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reviews` (
-  `repo` varchar(255) NOT NULL,
+CREATE TABLE IF NOT EXISTS `reviews` (
+  `repo` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `review_id` int unsigned NOT NULL,
   `number` int unsigned NOT NULL,
-  `body` text,
-  `state` varchar(255) NOT NULL,
-  `user` varchar(255) NOT NULL,
+  `body` text COLLATE utf8mb4_general_ci,
+  `state` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `user` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `date` int unsigned DEFAULT NULL,
   PRIMARY KEY (`repo`,`review_id`),
   KEY `pull` (`number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50112 SET @disable_bulk_load = IF (@is_rocksdb_supported, 'SET SESSION rocksdb_bulk_load = @old_rocksdb_bulk_load', 'SET @dummy_rocksdb_bulk_load = 0') */;
 /*!50112 PREPARE s FROM @disable_bulk_load */;
