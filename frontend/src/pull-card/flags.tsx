c062624e1269219b33809d8d29ea7ad9035fc0b2
Daniel Beardsley <daniel@ifixit.com>
2022-03-13 16:30:34 -0600

    pull-card: add dev_blocked flag

--- frontend/src/pull-card/flags.tsx
+++ frontend/src/pull-card/flags.tsx
@@ -1,22 +1,46 @@
 import { Pull } from '../pull';
 import { Link } from "@chakra-ui/react"
+import {
+    IconDefinition,
+} from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWarning } from '@fortawesome/free-solid-svg-icons'
+import { faWarning, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
 
 export function Flags({pull}: {pull: Pull}) {
+   const devBlock = pull.getDevBlock();
+   const deployBlock = pull.getDeployBlock();
    return (<>
-      {pull.hasDeployBlock() && <Link
-         href={pull.linkToSignature(pull.status.deploy_block[0])}
-         color="var(--tag-deploy-blocked)"
-         py="5px"
-         px="6px"
-         lineHeight="1em"
-         className="pull-flag"
-         backgroundColor="var(--tag-deploy-blocked-background)"
-         border="var(--tag-deploy-blocked-border) solid 1px"
-         borderRadius="3px"><FontAwesomeIcon
-            fontSize="18px"
-            icon={faWarning}/>
-      </Link>}
+      {deployBlock && <PullFlag
+         href={pull.linkToSignature(deployBlock)}
+         cssVarPrefix="--tag-deploy-blocked"
+         icon={faWarning}
+      />}
+      {devBlock && <PullFlag
+         href={pull.linkToSignature(devBlock)}
+         cssVarPrefix="--tag-dev-blocked"
+         icon={faMinusCircle}
+      />}
    </>);
+}
+
+interface PullFlagProps {
+   href: string,
+   icon: IconDefinition;
+   cssVarPrefix: string;
+}
+
+function PullFlag({href, icon, cssVarPrefix}: PullFlagProps) {
+   return (<Link
+      href={href}
+      color={`var(${cssVarPrefix})`}
+      py="5px"
+      px="6px"
+      lineHeight="1em"
+      className="pull-flag"
+      backgroundColor={`var(${cssVarPrefix}-background)`}
+      border={`var(${cssVarPrefix}-border) solid 1px`}
+      borderRadius="3px"><FontAwesomeIcon
+         fontSize="18px"
+         icon={icon}/>
+   </Link>);
 }
