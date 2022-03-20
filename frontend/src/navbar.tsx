30a915e596c744aa10177e4faab31e836e3e94c0
Daniel Beardsley <daniel@ifixit.com>
2022-03-20 14:47:09 -0600

    navbar: refactor layout
    We'll want to put more stuff up here, so use some enclosing Box
    components.

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -1,6 +1,6 @@
 import { usePulls, useSetFilter } from './pulldasher/pulls-context';
 import { Pull } from './pull';
-import { Center, Flex, Box, BoxProps, Input } from "@chakra-ui/react"
+import { HStack, Center, Flex, Box, BoxProps, Input } from "@chakra-ui/react";
 
 export function Navbar(props: BoxProps) {
    const pulls: Pull[] = usePulls();
@@ -19,9 +19,13 @@ export function Navbar(props: BoxProps) {
    return (
       <Center py={2} bgColor="var(--header-background)" {...props}>
          <Flex px="var(--body-gutter)" w={1024} justify="space-between">
-            <Box alignSelf="center" w={150}>{pulls.length} open</Box>
+            <HStack alignSelf="center" w="200px" spacing="2">
+               <span>{pulls.length} open</span>
+            </HStack>
             <Box alignSelf="center" fontSize={20} __css={{fontVariantCaps: "small-caps"}}>Pulldasher</Box>
-            <Input alignSelf="center" w={150} onChange={updateSearchFilter} placeholder="Search"/>
+            <Box w="200px" textAlign="right">
+               <Input w={150} onChange={updateSearchFilter} placeholder="Search"/>
+            </Box>
          </Flex>
       </Center>
    );
