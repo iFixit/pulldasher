d9a2a1646e9d955a3308d423bf73b6aa80e81e88
Daniel Beardsley <daniel@ifixit.com>
2022-03-21 01:24:20 -0600

    Pull-card: add button to copy the branch name
    It's often useful to get the branch name when looking at a pull.
    Now you can just click a button!

---
+++ frontend/src/pull-card/copy-branch.tsx
@@ -0,0 +1,27 @@
+import { useClipboard, chakra } from "@chakra-ui/react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy, faCheck } from '@fortawesome/free-solid-svg-icons'
+
+const CopyBranchIcon = chakra(FontAwesomeIcon, {
+   baseStyle: {
+      marginLeft: "10px",
+      cursor: "pointer",
+      color: "var(--copy-branch)",
+      "&:hover": {
+         color: "var(--copy-branch-hover)",
+      },
+   }
+});
+
+export function CopyBranch({className, value}: {className:string, value:string}) {
+   const {onCopy, hasCopied} = useClipboard(value);
+   return (
+      <CopyBranchIcon
+         title="Copy branch name"
+         size="lg"
+         className={className}
+         onClick={onCopy}
+         icon={hasCopied ? faCheck : faCopy}
+      />
+   );
+}
