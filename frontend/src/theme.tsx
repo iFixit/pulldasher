6b26070f54d85728a71ebfa3f3860b9835b1a4fd
Daniel Beardsley <daniel@ifixit.com>
2022-03-28 09:52:35 -0600

    Dark use light-mode initially
    Also, don't try to use the system preference. I'd rather have everyone
    start on light-mode and switch if they want.

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -1,6 +1,8 @@
 import { extendTheme } from "@chakra-ui/react"
 
 export const theme = extendTheme({
+   initialColorMode: 'light',
+   useSystemColorMode: false,
    components: {
       Link: {
          baseStyle: {
