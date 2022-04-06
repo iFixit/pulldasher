2c40bc931321b5e5eba9e8412950c7d53467894c
Daniel Beardsley <daniel@ifixit.com>
2022-04-05 21:37:13 -0600

    Theme: move theme files from old dir to new frontend
    Add them to theme.tsx so that one import will give everything needed.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,9 +3,6 @@ import { Pulldasher } from './pulldasher';
 import { PullsProvider } from './pulldasher/pulls-context';
 import { ChakraProvider } from "@chakra-ui/react"
 import { theme } from './theme';
-import './theme/base.less';
-import '../../views/standard/less/themes/day_theme.less';
-import '../../views/standard/less/themes/night_theme.less';
 
 const root = document.createElement("div");
 document.body.appendChild(root);
--- frontend/test/pull-card-demo.tsx
+++ frontend/test/pull-card-demo.tsx
@@ -19,8 +19,6 @@ import { PullCard } from '../src/pull-card';
 import { render } from 'react-dom';
 import { ChakraProvider } from "@chakra-ui/react"
 import { theme } from '../src/theme';
-import '../../views/standard/less/themes/day_theme.less';
-import '../../views/standard/less/themes/night_theme.less';
 
 const root = document.createElement("div");
 document.body.appendChild(root);
