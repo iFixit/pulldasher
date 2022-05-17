8ac0ed622ef98c126ac53832fee9b2549a4766a2
Daniel Beardsley <daniel@ifixit.com>
2022-05-17 12:59:45 -0700

    Chakra: get rid of warning from Popper
    Popper warns that it's not OK to use margin in some instance where it's
    getting used. Chakra is doing this, not us. There's an issue and the
    workaround is easy for now: wrap the menu in a Box:
    
    https://github.com/chakra-ui/chakra-ui/issues/3173

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -57,8 +57,12 @@ export function Navbar(props: BoxProps) {
                   onClick={toggleColorMode}>
                   <FontAwesomeIcon icon={faMoon}/>
                </Button>
-               <FilterMenu urlParam="repo" buttonText="Repo" extractValueFromPull={(pull: Pull) => pull.getRepoName()}/>
-               <FilterMenu urlParam="author" buttonText="Author" extractValueFromPull={(pull: Pull) => pull.user.login}/>
+               <Box>
+                  <FilterMenu urlParam="repo" buttonText="Repo" extractValueFromPull={(pull: Pull) => pull.getRepoName()}/>
+               </Box>
+               <Box>
+                  <FilterMenu urlParam="author" buttonText="Author" extractValueFromPull={(pull: Pull) => pull.user.login}/>
+               </Box>
             </HStack>
             <Flex alignSelf="center" fontSize={20} flexShrink={0}>
                <span style={{fontVariantCaps: "small-caps"}}>Pulldasher</span>
