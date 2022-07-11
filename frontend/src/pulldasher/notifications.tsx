9456617376afc5226d50d370c176e5c80b0c2528
Andrew Gilbert <andrewg@ifixit.com>
2022-07-11 16:13:31 -0700

    Notifications: Only track relevant pulls in each column
    Previously, we were including all of the user's PRs in the list which
    would trigger notifications in the CR or QA column. We don't really
    want that, for two reasons:
    1. It notifies when they go from CR-complete to needs-CR
    2. It doesn't notify when a self-QA expires and needs to be updated.
    
    This now splits out two hooks: one for pulls which need re-review, and
    one for pulls which are mine. We then use those as appropriate to
    trigger the notifications.

--- frontend/src/pulldasher/notifications.tsx
+++ frontend/src/pulldasher/notifications.tsx
@@ -2,17 +2,29 @@ import { useNotification } from "../notifications";
 import { getUser } from "../page-context";
 import { Pull } from "../pull";
 
-export function usePullNotification(pulls: Pull[], action: string) {
+export function usePullNotification(
+  pulls: Pull[],
+  action: string,
+  filter: (pull: Pull) => boolean
+) {
   useNotification<Pull>(pulls, {
     key: (pull: Pull) => pull.number,
-    filter: (pull: Pull) => pull.isMine() || pull.hasOutdatedSig(getUser()),
+    filter,
     message(pulls) {
       const titles = pulls.map((p) => p.title).join(", ");
-      console.log(action, titles);
       if (pulls.length === 1) {
         return `Pull ready for ${action}: ` + titles;
       }
       return `Pulls ready for ${action}: ${titles}`;
     },
   });
+}
+
+export function useMyPullNotification(pulls: Pull[], action: string) {
+  usePullNotification(pulls, action, (pull: Pull) => pull.isMine());
+}
+export function useMyReviewNotification(pulls: Pull[], action: string) {
+  usePullNotification(pulls, action, (pull: Pull) =>
+    pull.hasOutdatedSig(getUser())
+  );
 }
