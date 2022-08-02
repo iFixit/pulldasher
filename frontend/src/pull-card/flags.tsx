42ee2c56a5266bc945f5849af808205253d19e19
jordycosta <jordy@ifixit.com>
2022-08-02 14:01:20 -0700

    Add new flag for draft state
    This commit adds in a new flag for pull requests that are in a draft
    state.

--- frontend/src/pull-card/flags.tsx
+++ frontend/src/pull-card/flags.tsx
@@ -4,11 +4,12 @@ import { Link, Box, useStyleConfig } from "@chakra-ui/react"
 import { memo } from "react"
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWarning, faMinusCircle, faEye, faEyeSlash, faSnowflake } from '@fortawesome/free-solid-svg-icons'
+import { faWarning, faMinusCircle, faEye, faEyeSlash, faSnowflake, faMinusSquare } from '@fortawesome/free-solid-svg-icons'
 
 export const Flags = memo(
 function Flags({pull}: {pull: Pull}) {
    const devBlock = pull.getDevBlock();
+   const draftBlock = pull.isDraft();
    const readyButNoCI = pull.isReady() && !pull.isCiRequired();
    const deployBlock = pull.getDeployBlock();
    const QAing = pull.getLabel("QAing");
@@ -30,6 +31,11 @@ function Flags({pull}: {pull: Pull}) {
          variant="devBlock"
          title={actionMessage('Dev blocked', devBlock.data.created_at, devBlock.data.user.login)}
          href={pull.linkToSignature(devBlock)}
+         icon={faMinusCircle}
+      />}
+      {draftBlock && <PullFlag
+         variant="draftBlock"
+         title={"Draft"}
          icon={faMinusCircle}
       />}
       {QAing && <PullFlag
