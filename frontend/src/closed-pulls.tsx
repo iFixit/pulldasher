5e060bb2ab3a6d010cdbb02b05891c4361aee44b
Daniel Beardsley <daniel@ifixit.com>
2022-12-11 12:12:11 -0800

    ClosedPulls: reformat props list

--- frontend/src/closed-pulls.tsx
+++ frontend/src/closed-pulls.tsx
@@ -7,7 +7,13 @@ export function ClosedPulls({onClickClose}: {onClickClose: () => void}) {
    const closedPulls: Pull[] = useFilteredPulls().filter(pull => pull.closed_at);
    const styles = useStyleConfig('Column', {variant: "closed"});
    return (
-      <Box __css={styles} position="absolute" right="0" top="70" bottom="0" width="300px" boxShadow="0px 0px 10px 0px #00000020">
+      <Box __css={styles}
+         position="absolute"
+         right="0"
+         top="70"
+         bottom="0"
+         width="300px"
+         boxShadow="0px 0px 10px 0px #00000020">
          <Flex className="column_header" onClick={onClickClose}>
             <Box p={3} pl={4}>Recently Closed Pulls</Box>
             <Spacer/>
