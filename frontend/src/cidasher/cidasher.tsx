113c3c1d9d912a13e0bf160d7b371c45d8115590
Daniel Beardsley <daniel@ifixit.com>
2023-01-15 10:56:55 -0800

    CI Lanes: sort by time and make them links
    New statuses come in at the top and now each links to it's corresponding
    target url.

--- frontend/src/cidasher/cidasher.tsx
+++ frontend/src/cidasher/cidasher.tsx
@@ -1,10 +1,11 @@
 import { useAllPulls } from '../pulldasher/pulls-context';
 import { Pull } from '../pull';
 import { Fragment } from 'react';
-import { Box, Flex, VStack, useStyleConfig } from "@chakra-ui/react"
+import { LinkBox, LinkOverlay, Box, Flex, VStack, useStyleConfig } from "@chakra-ui/react"
 import { CommitStatus } from "../types"
 import { useUrlState } from "../use-url-state"
 import { useDurationMinutes } from "../utils";
+import { sortBy } from 'lodash-es'
 
 export function CiDasher() {
    const [repo] = useUrlState('ci-repo', "iFixit/ifixit");
@@ -31,11 +32,12 @@ type CILaneProps = {
 
 function CILane({context, statuses}: CILaneProps) {
    const styles = useStyleConfig('CILane');
+   const sorted = sortBy(statuses, (status) => (-(status.data.started_at || Date.now()/1000)));
    return (
       <Box __css={styles} flex={100}>
          <Box className="lane-header">{context}</Box>
          <VStack align="stretch">
-            {statuses.map((status, index) => (
+            {sorted.map((status, index) => (
                <CICard key={index} status={status}/>
             ))}
          </VStack>
@@ -46,14 +48,17 @@ function CILane({context, statuses}: CILaneProps) {
 function CICard({status}: {status: CommitStatus}) {
    const styles = useStyleConfig('CICard', {variant: status.data.state});
    const duration = useDurationMinutes(status);
-   return <Box
+   return <LinkBox>
+   <Box
       __css={styles}
       h={((duration||0) + 5) * 10 + "px"}
       p={2}
    >
+      {status.data.target_url && <LinkOverlay href={status.data.target_url}/>}
       {duration &&
       <Box className="ci-duration">{Math.ceil(duration)}m</Box>}
-   </Box>;
+   </Box>
+   </LinkBox>;
 }
 
 type StatusesByContext = Map<string, CommitStatus[]>;
