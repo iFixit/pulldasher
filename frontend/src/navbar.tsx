f3aa94d7fa4b058c693347d4257cd60e3fc0437a
jordycosta <jordy@ifixit.com>
2023-03-21 12:55:38 -0700

    specify conditional more
    This updates the .includes() method to search for -dev instead of just
    dev
    
    Having the search be for dev is too broad and would be triggered by
    clicking on the Dev Block column

--- frontend/src/navbar.tsx
+++ frontend/src/navbar.tsx
@@ -37,7 +37,7 @@ import {
 
 let title = 'Pulldasher';
 
-if (document.URL.includes('dev')) {
+if (document.URL.includes('-dev')) {
   document.title = 'Pulldasher-Dev';
   title = 'Pulldasher-Dev';
 }
