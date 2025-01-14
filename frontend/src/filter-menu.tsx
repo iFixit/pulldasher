03fedab46318e0a2d9d6ee2a2558b25f73331598
Daniel Beardsley <daniel@ifixit.com>
2025-01-14 14:20:11 -0800

    FilterMenu: support a "defaultExcludedValues" prop
    It took a long time to get this to behave intuitively. In short, it took
    a long to spec it out. How to have it behave when you click one of the
    values, how to represent the default state, ...
    
    The feature as implemented:
    
    * FilterMenu has a new prop defaultExcludedValues: string[]
    * Without defaultExcludedValues set, it operates exactly as it does now
    * When set, the UI has one new "showDefault" state in addition to
      existing "showAll"
    * In "showDefault", Pulls matching one of the excluded values are hidden
      (these excluded values appear de-selected in the UI)
      * One click "selects" them and leaves "showDefault" mode
    * "Show All" overrides the "default exclusions" and shows everything

--- frontend/src/filter-menu.tsx
+++ frontend/src/filter-menu.tsx
@@ -17,22 +17,29 @@ import { countBy } from "lodash-es";
 // Map from value to number of pulls that have that value
 type ValueGetter = (pull: Pull) => string;
 
+const SHOWALL = "SHOWALL";
+
 type FilterMenuProps = {
   urlParam: string;
   buttonText: string;
   extractValueFromPull: ValueGetter;
+  defaultExculdedValues?: string[];
 };
 
 export function FilterMenu({
   urlParam,
   buttonText,
   extractValueFromPull,
+  defaultExculdedValues,
 }: FilterMenuProps) {
   const pulls = useAllOpenPulls();
   // Default is empty array that implies show all pulls (no filtering)
   const [selectedValues, setSelectedValues] = useArrayUrlState(urlParam, []);
-  // Nothing selected == show everything, otherwise, it'd be empty
-  const showAll = selectedValues.length === 0;
+  // Nothing selected == show the default values (everything except the excluded values)
+  const showDefault = selectedValues.length === 0;
+  // Show every single value if the magic SHOWALL string is selected
+  const showAll = notEmpty(defaultExculdedValues) ? selectedValues.includes(SHOWALL) : selectedValues.length === 0;
+
   // List from url may contain values we have no pulls for
   const urlValues = useConst(() => new Set(selectedValues));
   const setPullFilter = useSetFilter();
@@ -41,20 +48,26 @@ export function FilterMenu({
   const allValues = useMemo(() => {
     // All values of open pulls
     const pullValues = new Set<string>(pulls.map(extractValueFromPull));
-    return sortValues([...new Set([...pullValues, ...urlValues])]);
+    const allValuesSet = new Set([...pullValues, ...urlValues]);
+    allValuesSet.delete(SHOWALL);
+    return sortValues([...allValuesSet]);
   }, [pulls]);
+
   const valueToPullCount = useMemo(
     () => countBy(pulls, extractValueFromPull),
     [pulls]
   );
 
+  const defaultSelectedValues = arrayDiff(allValues, defaultExculdedValues || []);
+
   useEffect(() => {
     const selectedValuesSet = new Set(selectedValues);
     setPullFilter(
       urlParam,
-      selectedValuesSet.size === 0
+      showAll
         ? null
-        : (pull) => selectedValuesSet.has(extractValueFromPull(pull))
+        : (showDefault ? (pull) => !defaultExculdedValues?.includes(extractValueFromPull(pull))
+          : (pull) => selectedValuesSet.has(extractValueFromPull(pull)))
     );
   }, [selectedValues]);
 
@@ -64,22 +77,42 @@ export function FilterMenu({
         as={Button}
         colorScheme="blue"
         size="sm"
-        variant={showAll ? "outline" : null}
+        variant={showDefault ? "outline" : null}
       >
         {buttonText} {selectedValues.length ? `(${selectedValues.length})` : ""}
       </MenuButton>
       <MenuList minWidth="240px">
-        <MenuItemOption
-          key="Show All"
-          onClick={() => setSelectedValues([])}
-          isChecked={showAll}
-        >
-          Show All
-        </MenuItemOption>
+        {notEmpty(defaultExculdedValues) && (
+          <>
+            <MenuItemOption
+              key="Show All"
+              onClick={() => setSelectedValues([SHOWALL])}
+              isChecked={showAll}
+            >
+              Show All
+            </MenuItemOption>
+            <MenuItemOption
+              key="Show All"
+              onClick={() => setSelectedValues([])}
+              isChecked={showDefault}
+            >
+              Show Default
+            </MenuItemOption>
+          </>)
+        }
+        {empty(defaultExculdedValues) &&
+          <MenuItemOption
+            key="Show All"
+            onClick={() => setSelectedValues([])}
+            isChecked={showAll}
+          >
+            Show All
+          </MenuItemOption>
+        }
         <MenuDivider />
         <MenuOptionGroup
           type="checkbox"
-          value={showAll ? [] : selectedValues}
+          value={showAll ? [] : (showDefault ? defaultSelectedValues : selectedValues)}
           onChange={setSelectedValues}
         >
           {allValues.map((value) => (
@@ -97,4 +130,18 @@ function sortValues(values: string[]): string[] {
   return values.sort((a: string, b: string) =>
     a.localeCompare(b, undefined, { sensitivity: "base" })
   );
+}
+
+function arrayDiff<T>(a: T[], b: T[]): T[] {
+  const ret = a.filter((x) => !b.includes(x));
+   console.log(ret);
+   return ret;
+}
+
+function empty<T>(array: T[] | undefined): boolean {
+  return !array || array.length === 0;
+}
+
+function notEmpty<T>(array: T[] | undefined): boolean {
+  return !!array && array.length > 0;
 }
